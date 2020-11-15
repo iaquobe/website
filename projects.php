9419c546c77faf8486b4ed3170826fd141f21cf0
jakob <jakob.lamberthartmann.canada@gmail.com>
2020-11-15 15:06:41 +0100

    added project subsite

---
+++ projects.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<?php 
+readfile("./header.html");
+?>
+
+<div id="content">
+	<h3 style="padding-top: 5%">$ls ~/projects</h3>
+	<img src="gif/banner/pokemon.gif" alt="retro ride" class="banner">
+
+	<h3>$news</h3>
+	<p>
+		human music now uploads every 1st day of the month.
+	</p>
+
+	<h3>$ls ~/projects</h3>
+	<p>
+		spotigrep: cli to search for lyrics in your spotify playlists<br>
+		human music: artificial inteligence to produce music. Results are published to: <a href=https://www.youtube.com/channel/UCDfNLU_M0MK3WOsIsScWirw>Youtube<a>
+	</p>
+
+
+	<span class=bottom-banner>
+		<img class=bottom-banner-left src=./gif/random/statue3.gif style="width:10%;">
+		<h3 class=bottom-banner-mid>EOF</h3>
+		<img class=bottom-banner-right src=./gif/random/statue3.gif style="width:10%;">
+	</span>
+</div>
+<?php 
+	readfile("./footer.html");
+?>
