cd746c1a17809b56f8a25de8e0423bc36a9e6de4
jakob <jakob.lamberthartmann.canada@gmail.com>
2021-07-28 22:47:34 +0200

    changed dockerfile

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM nginx
-
-RUN apt-get -y update && apt -y install php php-cli php-fpm
-
-COPY ./html /usr/share/nginx/html
-
-CMD service /usr/sbin/php7.4-fpm start && service nginx start && tail -f /dev/null
---
+++ start_docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+script_dir="$( cd -- "$(dirname "$0")"; pwd -P)"
+echo starting website 
+
+docker run -d -it \
+	-p 80:80
+	-v "$script_dir:/app" \
+	--name website
+	-e EULA=TRUE webdevops/php-nginx
